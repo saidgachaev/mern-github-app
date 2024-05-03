df9f54dec398cc24c0bc88131b4e813059f2a79f
someonempty <someonempty@gmail.com>
2024-05-03 22:46:12 +0300

    Adding UI Design.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import ExplorePage from "./pages/ExplorePage";
+import LikesPage from "./pages/LikesPage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import HomePage from "./pages/HomePage";
+
 function App() {
-
-  return (
-    <>
-     hey
-    </>
-  )
+	return (
+		<div className='flex'>
+			<Sidebar />
+			<div className='max-w-5xl my-5 text-white mx-auto transition-all duration-300 flex-1'>
+				<Routes>
+					<Route path='/' element={<HomePage />} />
+					<Route path='/login' element={<LoginPage />} />
+					<Route path='/signup' element={<SignUpPage />} />
+					<Route path='/explore' element={<ExplorePage />} />
+					<Route path='/likes' element={<LikesPage />} />
+				</Routes>
+			</div>
+		</div>
+	);
 }
-
-export default App
+export default App;
