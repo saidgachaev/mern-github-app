4cbc781613c835131e2ec1b5db11b742a4fc75a6
someonempty <someonempty@gmail.com>
2024-05-04 16:20:06 +0300

    Integrating Github Api.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
-import { Route, Routes } from "react-router-dom";
-import Sidebar from "./components/Sidebar";
-import ExplorePage from "./pages/ExplorePage";
-import LikesPage from "./pages/LikesPage";
-import LoginPage from "./pages/LoginPage";
-import SignUpPage from "./pages/SignUpPage";
-import HomePage from "./pages/HomePage";
+import { Route, Routes } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import HomePage from './pages/HomePage';
+import ExplorePage from './pages/ExplorePage';
+import LikesPage from './pages/LikesPage';
+
+import Sidebar from './components/Sidebar';
 
 function App() {
 	return (
@@ -18,8 +21,10 @@ function App() {
 					<Route path='/explore' element={<ExplorePage />} />
 					<Route path='/likes' element={<LikesPage />} />
 				</Routes>
+				<Toaster />
 			</div>
 		</div>
 	);
 }
+
 export default App;
