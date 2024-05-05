3b64b5b9ab7cc0a3eb84797e33091dc965dafd45
someonempty <someonempty@gmail.com>
2024-05-05 13:18:36 +0300

    Fixing Sidebar.

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -7,11 +7,12 @@ import { MdEditDocument } from 'react-icons/md';
 import Logout from './Logout';
 
 const Sidebar = () => {
-	const authUser = false;
+	const authUser = true;
+
 	return (
 		<aside
-			className='flex flex-col items-center min-w-12 sm:w-16 sticky top-0 left-0 h-screen py-8 overflow-y-auto border-r bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 
-		hover:bg-gray-600/10 border-gray-800 text-white'
+			className='flex flex-col items-center min-w-12 sm:w-16 sticky top-0 left-0 h-screen py-8
+      overflow-y-auto border-r bg-glass'
 		>
 			<nav className='h-full flex flex-col gap-3'>
 				<Link to='/' className='flex justify-center'>
@@ -38,7 +39,7 @@ const Sidebar = () => {
 				{authUser && (
 					<Link
 						to='/explore'
-						className='p-1.5  flex justify-center transition-colors duration-200 rounded-lg hover:bg-gray-800'
+						className='p-1.5 flex justify-center transition-colors duration-200 rounded-lg hover:bg-gray-800'
 					>
 						<MdOutlineExplore size={25} />
 					</Link>
@@ -61,6 +62,7 @@ const Sidebar = () => {
 						<MdEditDocument size={25} />
 					</Link>
 				)}
+
 				{authUser && (
 					<div className='flex flex-col gap-2 mt-auto'>
 						<Logout />
@@ -70,5 +72,4 @@ const Sidebar = () => {
 		</aside>
 	);
 };
-
 export default Sidebar;
